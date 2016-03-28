1d9607eb90bf4cf1a70f641a615ce6653d41de8e
Akshay Iyer <akshay.iyer@nyu.edu>
2016-03-27 22:18:19 -0400

    Completed Coursework, Challenges Remaining

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Akshay Iyer on 3/27/16.
+//  Copyright © 2016 akshaytiyer. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
